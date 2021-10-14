ba7c69c5229dafffbacb75fb149f60a74678a31c
Naman Seth <naman.seth_cs19@gla.ac.in>
2021-10-14 08:53:01 +0530

    add onload function

--- Assignment14/script.js
+++ Assignment14/script.js
@@ -1,2 +1,12 @@
 var count = 0;
 var valueChange = document.getElementById("counterValue");
+
+window.onload = () => {
+  if (count === 0) {
+    var bt = document.querySelector("button");
+    bt.disabled = true;
+  } else {
+    var bt = document.querySelector("button");
+    bt.disabled = false;
+  }
+};
