4c185a38e6ca8c98977f5e20912e1cf7b9c2c649
Naman Seth <naman.seth_cs19@gla.ac.in>
2021-10-14 08:53:20 +0530

    add increment dunction

--- Assignment14/script.js
+++ Assignment14/script.js
@@ -10,3 +10,14 @@ window.onload = () => {
     bt.disabled = false;
   }
 };
+
+function increment() {
+  if (count === 0) {
+    var bt = document.querySelector("button");
+    bt.disabled = true;
+  } else {
+    var bt = document.querySelector("button");
+    bt.disabled = false;
+  }
+  valueChange.innerHTML = count++;
+}
