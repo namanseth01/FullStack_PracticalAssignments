aa6ac0cc86c418a06a7791bb2265f5c2c04b8d83
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-19 21:01:36 +0530

    add function checkInputs

--- Assignment15/script.js
+++ Assignment15/script.js
@@ -8,3 +8,37 @@ form.addEventListener("submit", function (e) {
   e.preventDefault(); // data will not submit to server
   checkInputs();
 });
+
+function checkInputs() {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const password1Value = password1.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === "") {
+    showError(username, "Username can not be blank");
+  } else {
+    showSuccess(username);
+  }
+  if (emailValue === "") {
+    showError(email, "Email Id can not be blank");
+  } else if (!isEmailValid(emailValue)) {
+    showError(email, "Email is not Valid");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password1Value === "") {
+    showError(password1, "Password can not be blank");
+  } else {
+    showSuccess(password1);
+  }
+
+  if (password2Value === "") {
+    showError(password2, "Password can not be blank");
+  } else if (password2Value != password1Value) {
+    showError(password2, "Passwords not matched");
+  } else {
+    showSuccess(password2);
+  }
+}
