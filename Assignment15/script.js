2613ef87978c2ff018700240490a611375752696
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-19 21:00:37 +0530

    add variables

---
+++ Assignment15/script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
