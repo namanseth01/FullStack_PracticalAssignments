f6b058be18a85c5c62e4498055ab51460642aee9
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-19 21:00:56 +0530

    add eventListner

--- Assignment15/script.js
+++ Assignment15/script.js
@@ -3,3 +3,8 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password1 = document.getElementById("password1");
 const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // data will not submit to server
+  checkInputs();
+});
